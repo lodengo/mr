d679a472b76aa894a763b62d5a1c67446b0efd3b
lodengo <ddl5200@gmail.com>
2014-09-25 15:06:19 +0800

    map emit

--- test/worldCount.js
+++ test/worldCount.js
@@ -4,11 +4,10 @@ var fs = require('fs');
 var inputs = fs.readdirSync('./').map(fs.createReadStream);
 
 var worldCounter = new MapReduce({
-	map: function(chunk){
-		var self = this;
+	map: function(chunk){		
 		chunk.toString().split(/\W+|\d+/).forEach(function(world){			
-			world && self.emit(world.toLowerCase(), 1);
-		});
+			world && this.emit(world.toLowerCase(), 1);
+		}, this);
 	},
 	reduce: function(key, values){
 		return this.count(values);
